6ab93ead16ec1f90848eedce06773bd7a9396435
baristaner <legendbaris2003@gmail.com>
2024-01-05 15:32:59 +0300

    Implemented Service layer just for Projects Entity and few other changes

---
+++ src/FastWIN.API/Helper/Mappers/ProductMapper.cs
@@ -0,0 +1,19 @@
+﻿using fastwin.Entities;
+using fastwin.Requests;
+
+namespace fastwin.Helper.Mappers
+{
+    public static class ProductMapper
+    {
+        public static Product MapProduct(ProductRequest productRequest)
+        {
+            return new Product
+            {
+                Name = productRequest.Name,
+                Description = productRequest.Description,
+                Category = Enum.Parse<Category>(productRequest.Category),
+                LastUsageDate = productRequest.LastUsageDate,
+            };
+        }
+    }
+}
